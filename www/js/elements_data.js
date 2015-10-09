0cbf86a0b4abaa548c74e20c7c7a246f99b7f483
patriciohc <patriciohc.0@gmail.com>
2015-10-08 19:37:36 -0500

    Inicio de pryecto

---
+++ www/js/elements_data.js
@@ -0,0 +1,513 @@
+/*datos de los elementos quimicos*/
+var ELEMENTO = 0;
+var NUMERO = 1;
+var OPEN = 2;
+var CLOSE = 3;
+var OPERADOR = 3;
+
+var data_elements = {
+/* Elementos quimicos */
+    H: {
+        PA:1.0079,
+        NO:1,
+        tipo: ELEMENTO
+    },
+    He: {
+        PA:4.0026,
+        NO:2,
+        tipo: ELEMENTO
+    },
+    Li: {
+        PA:6.941,
+        NO:3,
+        tipo: ELEMENTO
+    },
+    Be: {
+        PA:9.0121,
+        NO:4,
+        tipo: ELEMENTO
+    },
+    B: {
+        PA:10.811,
+        NO:5,
+        tipo: ELEMENTO
+    },
+    C: {
+        PA:12.0107,
+        NO:6,
+        tipo: ELEMENTO
+    },
+    N: {
+        PA:14.0067,
+        NO:7,
+        tipo: ELEMENTO
+    },
+    O: {
+        PA:15.9994,
+        NO:8,
+        tipo: ELEMENTO
+    },
+    F: {
+        PA:18.9984,
+        NO:9,
+        tipo: ELEMENTO
+    },
+    Ne: { 
+        PA:20.1797,
+        NO:10,
+        tipo: ELEMENTO
+    },
+    Na: {
+        PA:22.9897,
+        NO:11,
+        tipo: ELEMENTO
+    },
+    Mg: {
+        PA:24.3050,
+        NO:12,
+        tipo: ELEMENTO
+    },
+    Al: {
+        PA:26.9815,
+        NO:13,
+        tipo: ELEMENTO
+    },
+    Si: {
+        PA:28.0855,
+        NO:14,
+        tipo: ELEMENTO
+    },
+    P: {
+        PA:30.9737,
+        NO:15,
+        tipo: ELEMENTO
+    },
+    S: {
+        PA:32.065,
+        NO:16,
+        tipo: ELEMENTO
+    },
+    Cl: {
+        PA:35.453,
+        NO:17,
+        tipo: ELEMENTO
+    },
+    Ar: {
+        PA:39.948,
+        NO:18,
+        tipo: ELEMENTO
+    },
+    K: {
+        PA:39.0983,
+        NO:19,
+        tipo: ELEMENTO
+    },
+    Ca: {
+        PA:40.078,
+        NO:20,
+        tipo: ELEMENTO
+    },
+    Sc: {
+        PA:44.9559,
+        NO:21,
+        tipo: ELEMENTO
+    },
+    Ti: {
+        PA:47.867,
+        NO:22,
+        tipo: ELEMENTO
+    },
+    V: {
+        PA:50.9415,
+        NO:23,
+        tipo: ELEMENTO
+    },
+    Cr: {
+        PA:51.9961,
+        NO:24,
+        tipo: ELEMENTO
+    },
+    Mn: {
+        PA:54.9380,
+        NO:25,
+        tipo: ELEMENTO
+    },
+    Fe: {
+        PA:55.845,
+        NO:26,
+        tipo: ELEMENTO
+    },
+    Co: {
+        PA:58.9332,
+        NO:27,
+        tipo: ELEMENTO
+    },
+    Ni: {
+        PA:58.6934,
+        NO:28,
+        tipo: ELEMENTO
+    },
+    Cu: {
+        PA:63.546,
+        NO:29,
+        tipo: ELEMENTO
+    },
+    Zn: {
+        PA:65.409,
+        NO:30,
+        tipo: ELEMENTO
+    },
+    Ga: {
+        PA:69.723,
+        NO:31,
+        tipo: ELEMENTO
+    },
+    Ge: {
+        PA:72.64,
+        NO:32,
+        tipo: ELEMENTO
+    },
+    As: {
+        PA:74.9216,
+        NO:33,
+        tipo: ELEMENTO
+    },
+    Se: {
+        PA:78.96,
+        NO:34,
+        tipo: ELEMENTO
+    },
+    Br: {
+        PA:79.904,
+        NO:35,
+        tipo: ELEMENTO
+    },
+    Kr: {
+        PA:83.796,
+        NO:36,
+        tipo: ELEMENTO
+    },
+    Rb: {
+        PA:85.4678,
+        NO:37,
+        tipo: ELEMENTO
+    },
+    Sr: {
+        PA:87.62,
+        NO:38,
+        tipo: ELEMENTO
+    },
+    Y: {
+        PA:88.9058,
+        NO:39,
+        tipo: ELEMENTO
+    },
+    Zr: {
+        PA:91.224,
+        NO:40,
+        tipo: ELEMENTO
+    },
+    Nb: {
+        PA:92.9063,
+        NO:41,
+        tipo: ELEMENTO
+    },
+    Mo: {
+        PA:95.94,
+        NO:37,
+        tipo: ELEMENTO
+    },
+    Tc: {
+        PA:98,
+        NO:43,
+        tipo: ELEMENTO
+    },
+    Ru: {
+        PA:101.07,
+        NO:44,
+        tipo: ELEMENTO
+    },
+    Rh: {
+        PA:102.9055,
+        NO:45,
+        tipo: ELEMENTO
+    },
+    Pd: {
+        PA:106.42,
+        NO:46,
+        tipo: ELEMENTO
+    },
+    Ag: {
+        PA:107.8682,
+        NO:47,
+        tipo: ELEMENTO
+    },
+    Cd: {
+        PA:112.411,
+        NO:48,
+        tipo: ELEMENTO
+    },
+    In: {
+        PA:114.818,
+        NO:49,
+        tipo: ELEMENTO
+    },
+    Sn: {
+        PA:118.710,
+        NO:50,
+        tipo: ELEMENTO
+    },
+    Sb: {
+        PA:121.760,
+        NO:51,
+        tipo: ELEMENTO
+    },
+    Te: {
+        PA:127.60,
+        NO:52,
+        tipo: ELEMENTO
+    },
+    I: {
+        PA:126.9044,
+        NO:53,
+        tipo: ELEMENTO
+    },
+    Xe: {
+        PA:131.293,
+        NO:54,
+        tipo: ELEMENTO
+    },
+    Cs: {
+        PA:132.9054,
+        NO:55,
+        tipo: ELEMENTO
+    },
+    Ba: {
+        PA:137.327,
+        NO:56,
+        tipo: ELEMENTO
+    },
+    Hf: {
+        PA:178.49,
+        NO:72,
+        tipo: ELEMENTO
+    },
+    Ta: {
+        PA:180.9479,
+        NO:73,
+        tipo: ELEMENTO
+    },
+    W: {
+        PA:183.84,
+        NO:74,
+        tipo: ELEMENTO
+    },
+    Re: {
+        PA:186.207,
+        NO:75,
+        tipo: ELEMENTO
+    },
+    Os: {
+        PA:190.23,
+        NO:76,
+        tipo: ELEMENTO
+    },
+    Ir: {
+        PA:192.217,
+        NO:77,
+        tipo: ELEMENTO
+    },
+    Pt: {
+        PA:195.078,
+        NO:78,
+        tipo: ELEMENTO
+    },
+    Au: {
+        PA:196.9665,
+        NO:79,
+        tipo: ELEMENTO
+    },
+    Hg: {
+        PA:200.59,
+        NO:80,
+        tipo: ELEMENTO
+    },
+    Tl: {
+        PA:204.3833,
+        NO:81,
+        tipo: ELEMENTO
+    },
+    Pb: {
+        PA:207.2,
+        NO:82,
+        tipo: ELEMENTO
+    },
+    Bi: {
+        PA:208.9803,
+        NO:83,
+        tipo: ELEMENTO
+    },
+    Po: {
+        PA:209,
+        NO:84,
+        tipo: ELEMENTO
+    },
+    At: {
+        PA:210,
+        NO:85,
+        tipo: ELEMENTO
+    },
+    Rn: {
+        PA:222,
+        NO:86,
+        tipo: ELEMENTO
+    },
+    Fr: {
+        PA:223,
+        NO:87,
+        tipo: ELEMENTO
+    },
+    Ra: {
+        PA:226,
+        NO:88,
+        tipo: ELEMENTO
+    },
+    Rf: {
+        PA:261,
+        NO:104,
+        tipo: ELEMENTO
+    },
+    Db: {
+        PA:262,
+        NO:105,
+        tipo: ELEMENTO
+    },
+    Sg: {
+        PA:266,
+        NO:106,
+        tipo: ELEMENTO
+    },
+    Bh: {
+        PA:264,
+        NO:107,
+        tipo: ELEMENTO
+    },
+    Hs: {
+        PA:277,
+        NO:108,
+        tipo: ELEMENTO
+    },
+    Mt: {
+        PA:268,
+        NO:109,
+        tipo: ELEMENTO
+    },
+    Mv: {
+        PA:281,
+        NO:110,
+        tipo: ELEMENTO
+    },
+    Pl: {
+        PA:272,
+        NO:111,
+        tipo: ELEMENTO
+    },
+    Da: {
+        PA:277,
+        NO:112,
+        tipo: ELEMENTO
+    },
+    Tf: {
+        PA:272,
+        NO:113,
+        tipo: ELEMENTO
+    },
+    Eo: {
+        PA:285,
+        NO:114,
+        tipo: ELEMENTO
+    },
+
+    Me: {
+        PA:279,
+        NO:115,
+        tipo: ELEMENTO
+    },
+    Nc: {
+        PA:289,
+        NO:116,
+        tipo: ELEMENTO
+    },
+    El: {
+        PA:286,
+        NO:117,
+        tipo: ELEMENTO
+    },
+    On: {
+        PA:288,
+        NO:118,
+        tipo: ELEMENTO
+    },
+
+/* Operadores */
+    MULT: {
+        valor: '*',
+        tipo: OPERADOR
+    },
+    DIV: {
+        valor: '/',
+        tipo: OPERADOR
+    },
+
+/* Numeros */
+    '0': {
+        valor: 0,
+        tipo: NUMERO
+    },
+    '1': {
+        valor: 1,
+        tipo: NUMERO
+    },
+    '2': {
+        valor: 2,
+        tipo: NUMERO
+    },
+    '3': {
+        valor: 3,
+        tipo: NUMERO
+    },
+    '4': {
+        valor: 4,
+        tipo: NUMERO
+    },
+    '5': {
+        valor: 5,
+        tipo: NUMERO
+    },
+    '6': {
+        valor: 6,
+        tipo: NUMERO
+    },
+    '7': {
+        valor: 7,
+        tipo: NUMERO
+    },
+    '8': {
+        valor: 8,
+        tipo: NUMERO
+    },
+    '9': {
+        valor: 9,
+        tipo: NUMERO
+    },
+/* Parentesis */
+    OPEN: {
+        valor: '(',
+        tipo: OPEN
+    },
+    CLOSE: {
+        valor: ')',
+        tipo: CLOSE
+    }
+}
+
---
+++ www/js/myindex.js
@@ -0,0 +1,188 @@
+
+var operacion = Array(20);
+var index_operacion = 0;
+var shows;
+
+function clear()
+{
+    for (var i = 0; i < index_operacion; i++) {
+        espacio = shows[i];
+        espacio.getElementsByTagName('span')[0].innerHTML = "";
+    }
+    index_operacion = 0;
+}
+
+function show(elemento)
+{
+    espacio = shows[index_operacion];
+    if (elemento == 'DIV') {
+        espacio.getElementsByTagName('span')[0].innerHTML =  "/";
+    } else if (elemento == 'MULT') {
+        espacio.getElementsByTagName('span')[0].innerHTML =  "*";
+    } else {
+        espacio.getElementsByTagName('span')[0].innerHTML =  elemento;
+    }
+}
+
+function del()
+{
+    espacio = shows[--index_operacion];
+    espacio.getElementsByTagName('span')[0].innerHTML = "";
+}
+    
+
+function agregar_elemento(name_elemento)
+{
+    show(name_elemento);
+    if (name_elemento == '(') {
+        name_elemento = 'OPEN';
+    } else if (name_elemento == ')') {
+        name_elemento = 'CLOSE'
+    }
+    var data_this_element = data_elements[name_elemento];
+    operacion[index_operacion++] = data_this_element;
+}
+
+
+function sumar_elementos(array_elementos, len)
+{
+    var i;
+    var sum = 0;
+    for (i = 0; i < len; i++) {
+        sum += array_elementos[i];
+    }
+    return sum;
+}
+
+function get_value_numeric(ini,end)
+{
+    var tmp = 0;
+    var digito;
+    var suma = 0;
+    var len = end - ini;
+    for (var i = 0; i <= len; i++) {
+        digito = operacion[end-i].valor;
+        tmp = Math.pow(10,i);
+        suma += digito * tmp;
+    }
+    return suma;
+}
+
+function suma_interna(init, end)
+{
+    var elemento;
+    var acumulador = Array(10);
+    var index_acumulador = 0;
+    for (var i = init; i < end; i++) {
+        elemento = operacion[i];
+        if (elemento.tipo == ELEMENTO) {
+            acumulador[index_acumulador++] = elemento.PA;    
+        } else if (elemento.tipo == NUMERO) {
+            elemento_mult = acumulador[index_acumulador-1];
+            acumulador[index_acumulador - 1] = elemento_mult * elemento.valor;
+        }        
+    }
+    return sumar_elementos(acumulador, index_acumulador)
+}
+
+function calcular()
+{
+    var elemento;
+    var acumulador = Array(10);
+    var index_acumulador = 0;
+    var numero = 1;
+    var operando1 = 0;
+    var operando2 = 0;
+    var operador = '';
+    for (var i = 0; i < index_operacion; i++) {
+        elemento = operacion[i];
+        switch (elemento.tipo) {
+        case ELEMENTO:
+            acumulador[index_acumulador++] = elemento.PA;
+            break;
+        case NUMERO:
+            var ini = i;
+            while (++i < index_operacion && operacion[i].tipo == NUMERO);
+            i -= 1;
+            numero = get_value_numeric(ini,i);
+            if (index_acumulador != 0) {
+                elemento_mult = acumulador[index_acumulador - 1];
+                acumulador[index_acumulador - 1] = elemento_mult * numero;
+                numero = 1;
+            }
+            break;
+        case OPERADOR:
+            operador = elemento.valor;
+            operando1 = sumar_elementos(acumulador, index_acumulador);
+            operando1 *= numero;
+            index_acumulador = 0;
+            numero = 1;
+            break;
+        case OPEN:
+            var ini = i;
+            while (operacion[++i].tipo != CLOSE);
+            var end = i;
+            acumulador[index_acumulador++] = suma_interna(ini,end);
+            break;
+        default:
+            alert("error de sintaxis");
+            return;
+        }
+    }
+    operando2 = sumar_elementos(acumulador, index_acumulador);
+    operando2 *= numero;
+    switch (operador) {
+    case '/':
+        if (operando2 == 0) alert("division entre 0!");
+        else resultado = operando1 / operando2;
+        break;
+    case '*':
+        resultado = operando1 * operando2;
+        break;    
+    default:
+        resultado = operando1 + operando2;        
+    }
+    alert(resultado);
+    /*clear();*/
+    /*index_operacion = 0;*/
+    return resultado;
+}
+
+function add_event(elementos)
+{
+    var v = 0;
+    var elemento;
+    var name_elemento;
+    var charts;
+    var f;
+
+    for (var i = 0; i<elementos.length;i++) {
+        elemento = elementos[i];
+        name_elemento = elemento.getElementsByTagName('span')[0].childNodes[0].nodeValue;
+        f = "agregar_elemento('"+name_elemento+"');";
+        elemento.setAttribute('onclick',f);
+        }
+}
+
+function ele()
+{
+    var elementos;
+    var elemento;
+    var bt_igual  = document.getElementById('igual');
+    var bt_delete = document.getElementById('delete');
+    var bt_clear = document.getElementById('clear');
+    
+    bt_igual.addEventListener('click', calcular, false);
+    bt_delete.addEventListener('click', del, false);
+    bt_clear.addEventListener('click', clear, false);
+    
+    elementos = document.getElementsByClassName('elemento');
+    add_event(elementos);
+    elementos = document.getElementsByClassName('numero');
+    add_event(elementos);
+    elementos = document.getElementsByClassName('operador');
+    add_event(elementos);
+    shows = document.getElementsByClassName('show'); /* espacios para mostrar resultados */
+}
+
+window.onload = ele;
